82bb7b129c55c8c16f9fb514aef9de48bcc7c62b
Rodrigo Croci Costa <rodrigo.croci@gmail.com>
2021-06-01 17:50:31 -0300

    Codando

--- WebAPI/src/DevIO.Api/Configuration/LoggerConfig.cs
+++ WebAPI/src/DevIO.Api/Configuration/LoggerConfig.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using Elmah.Io.AspNetCore;
+using Elmah.Io.Extensions.Logging;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace DevIO.Api.Configuration
 {
@@ -17,15 +18,15 @@ namespace DevIO.Api.Configuration
                 o.LogId = new Guid("fe2c0018-0b26-43d7-9c4e-d9920762411a");
             });
 
-            //services.AddLogging(builder =>
-            //{
-            //    builder.AddElmahIo(o =>
-            //    {
-            //        o.ApiKey = "388dd3a277cb44c4aa128b5c899a3106";
-            //        o.LogId = new Guid("c468b2b8-b35d-4f1a-849d-f47b60eef096");
-            //    });
-            //    builder.AddFilter<ElmahIoLoggerProvider>(null, LogLevel.Warning);
-            //});
+            services.AddLogging(builder =>
+            {
+                builder.AddElmahIo(o =>
+                {
+                    o.ApiKey = "364b5ae8b67a4f0f9bd2336cd96a2722";
+                    o.LogId = new Guid("fe2c0018-0b26-43d7-9c4e-d9920762411a");
+                });
+                builder.AddFilter<ElmahIoLoggerProvider>(null, LogLevel.Warning);
+            });
 
             //services.AddHealthChecks()
             //    .AddElmahIoPublisher("364b5ae8b67a4f0f9bd2336cd96a2722", new Guid("fe2c0018-0b26-43d7-9c4e-d9920762411a"),"API Fornecedores")
@@ -33,7 +34,7 @@ namespace DevIO.Api.Configuration
             //    .AddSqlServer(configuration.GetConnectionString("DefaultConnection"), name: "BancoSQL");
 
             //services.AddHealthChecksUI();
-             
+
             return services;
         }
 
